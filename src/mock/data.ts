06252958db21e69df8d9a008116888f1d2c83e84
Vuk Perovic <vukperovic03@gmail.com>
2026-02-07 18:50:27 +0100

    Add types and mock data

--- src/mock/data.ts
+++ src/mock/data.ts
@@ -1,3 +1,38 @@
 /**
  * Data used for testing without a DB
  */
+
+import { Currency } from "@/shared/types";
+
+export const currencies: Array<Currency> = [
+  {
+    id: 1,
+    code: "RSD",
+    name: "Srpski dinar",
+    symbol: "RSD",
+  },
+  {
+    id: 2,
+    code: "EUR",
+    name: "Evro",
+    symbol: "€",
+  },
+  {
+    id: 3,
+    code: "GBP",
+    name: "Britanska funta",
+    symbol: "£",
+  },
+  {
+    id: 4,
+    code: "USD",
+    name: "Američki dolar",
+    symbol: "$",
+  },
+  {
+    id: 5,
+    code: "JPY",
+    name: "Japanski jen",
+    symbol: "¥",
+  },
+];
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,3 +1,94 @@
 /**
  * Definitions of types used by the frontend
  */
+
+// Enums
+export enum Sex {
+  MALE = "Muški",
+  FEMALE = "Ženski",
+}
+
+export enum AccountStatus {
+  ACTIVE = "Aktivan",
+  INACTIVE = "Neaktivan",
+  BLOCKED = "Blokiran",
+}
+
+export enum TransactionStatus {
+  PENDING = "Na čekanju",
+  EXECUTED = "Izvršena",
+  FAILED = "Neuspešna",
+}
+
+export enum ExpenseCategory {
+  HOUSING = "Stanovanje",
+  HOME = "Domaćinstvo",
+  FOOD = "Hrana",
+  SPORTS = "Sport",
+  LEISURE = "Zabava",
+  GOING_OUT = "Izlazak",
+  OTHER = "Ostalo",
+}
+
+// Types
+export type UserRole = "ADMIN" | "MANAGER" | "CLIENT";
+
+// Interfaces
+export interface Currency {
+  id: number;
+  code: string; // 'USD'
+  name: string; // 'American Dollar'
+  symbol: string; // '$'
+}
+
+export interface BaseUser {
+  id: number;
+  role: UserRole;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  nationalId: string;
+  dateOfBirth: Date;
+  sex: Sex;
+  createdAt: Date;
+  active: boolean;
+}
+
+export interface Administrator extends BaseUser {
+  role: "ADMIN";
+}
+
+export interface Manager extends BaseUser {
+  role: "MANAGER";
+  clientIDs: Array<number>;
+}
+
+export interface Client extends BaseUser {
+  role: "CLIENT";
+  managerID: number | null;
+}
+
+export interface Account {
+  id: number;
+  accountNo: string;
+  balance: number;
+  createdAt: Date;
+  status: AccountStatus;
+  clientID: number;
+  currency: Currency;
+  transactionIDs: Array<number>;
+}
+
+export interface Transaction {
+  id: number;
+  amount: number;
+  timestamp: Date;
+  accountSrcNo: string;
+  accountDestNo: string;
+  description: string | null;
+  status: TransactionStatus;
+  category: ExpenseCategory | null;
+  currency: Currency;
+}
